ca3df78d57c1839ca9ae132a021209eecabe888b
Vytautas Klumbys <Vyckaxxx@gmail.com>
2020-05-13 11:19:22 +0300

    Fix null-ref error on FuncHandlers.

--- src/HttpMockSlim/Extensions/HttpListenerRequestExtensions.cs
+++ src/HttpMockSlim/Extensions/HttpListenerRequestExtensions.cs
@@ -8,7 +8,7 @@ namespace HttpMockSlim.Extensions
         public static bool IsGZipped(this HttpListenerRequest request)
         {
             string content = request.Headers["Content-Encoding"];
-            return content.StartsWith("gzip", StringComparison.InvariantCulture);
+            return content?.StartsWith("gzip", StringComparison.InvariantCulture) == false;
         }
     }
 }
